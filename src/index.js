087b22b1100410602e7c4c6ae6c1aa7f9c00a6ec
KuilDi <kuildi@yandex.ru>
2021-11-06 13:28:32 +0300

    sprint-2/step-2 перенос создания store в отдельный файл

--- src/index.js
+++ src/index.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { compose, createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './components/app/app';
-import {rootReducer} from './services/reducers/root-reducer';
 import reportWebVitals from './reportWebVitals';
+import { store } from './services/store';
 
 import '@ya.praktikum/react-developer-burger-ui-components/dist/ui/fonts/fonts.css';
 import '@ya.praktikum/react-developer-burger-ui-components/dist/ui/common.css';
 import '@ya.praktikum/react-developer-burger-ui-components/dist/ui/box.css';
-
-
-const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
-
-const enhancer = composeEnhancers(applyMiddleware(thunk));
-
-const store = createStore(rootReducer, enhancer);
-
 
 ReactDOM.render(
   <React.StrictMode>
